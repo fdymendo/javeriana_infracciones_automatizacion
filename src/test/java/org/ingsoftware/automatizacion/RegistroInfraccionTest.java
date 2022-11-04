d1378c39f98e4f17b96bf861024f139fe76b7965
Alejandro Tovar <diego.tovar@globant.com>
2022-11-03 22:55:47 -0500

    Initial Commit

---
+++ src/test/java/org/ingsoftware/automatizacion/RegistroInfraccionTest.java
@@ -0,0 +1,61 @@
+package org.ingsoftware.automatizacion;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+
+public class RegistroInfraccionTest {
+
+    private WebDriver webDriver;
+
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", ".\\src\\test\\resources\\chromedriver\\chromedriver.exe");
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.get("http://localhost:3000/infraccion");
+    }
+
+    @Test
+    public void debeRegistrarInfraccionSatisfactoriamente() {
+
+        WebElement primeraBox = webDriver.findElement(By.id("nombretxt"));
+        primeraBox.sendKeys("Yilmer Patricio");
+
+        WebElement apellidoTxt = webDriver.findElement(By.id("apellidotxt"));
+        apellidoTxt.sendKeys("Fernancoz de la Roa");
+
+        WebElement emailTxt = webDriver.findElement(By.id("emailtxt"));
+        emailTxt.sendKeys("Yilmer@aol.com");
+
+        WebElement placaTxt = webDriver.findElement(By.id("placatxt"));
+        placaTxt.sendKeys("BBE332");
+
+        WebElement documentoTxt = webDriver.findElement(By.id("iddoctxt"));
+        documentoTxt.sendKeys("25147125");
+
+        Select infraccionSel = new Select(webDriver.findElement(By.id("inftxt")));
+        infraccionSel.selectByVisibleText("84c9dcbc-bae4-4fef-aa46-77541656cf95");
+
+        Select tipoDocumentoSel = new Select(webDriver.findElement(By.id("doctxt")));
+        tipoDocumentoSel.selectByVisibleText("CC");
+
+        WebElement enviarRegistro = webDriver.findElement(By.id("btnreg"));
+        enviarRegistro.click();
+
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("Toastify__toast-body")));
+
+        webDriver.quit();
+    }
+
+
+}
